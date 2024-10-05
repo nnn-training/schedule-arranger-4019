834a4517e85bcd3eab15cb975f9378f1365586dd
koppepam <rise.yoneyama@gmail.com>
2024-10-05 18:06:16 +0900

    シングルクォーテーションに統一

--- src/app.test.js
+++ src/app.test.js
@@ -1,22 +1,22 @@
-"use strict";
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient({ log: ["query"] });
+'use strict';
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient({ log: ['query'] });
 
 const testUser = {
   userId: 0,
-  username: "testuser",
+  username: 'testuser',
 };
 
 function mockIronSession() {
-  const ironSession = require("iron-session");
-  jest.spyOn(ironSession, "getIronSession").mockReturnValue({
+  const ironSession = require('iron-session');
+  jest.spyOn(ironSession, 'getIronSession').mockReturnValue({
     user: { login: testUser.username, id: testUser.userId },
     save: jest.fn(),
     destroy: jest.fn(),
   });
 }
 
-describe("/login", () => {
+describe('/login', () => {
   beforeAll(() => {
     mockIronSession();
   });
@@ -25,33 +25,33 @@ describe("/login", () => {
     jest.restoreAllMocks();
   });
 
-  test("ログインのためのリンクが含まれる", async () => {
-    const app = require("./app");
-    const res = await app.request("/login");
-    expect(res.headers.get("Content-Type")).toBe("text/html; charset=UTF-8");
-    expect(await res.text()).toMatch(/<a href="\/auth\/github"/);
+  test('ログインのためのリンクが含まれる', async () => {
+    const app = require('./app');
+    const res = await app.request('/login');
+    expect(res.headers.get('Content-Type')).toBe('text/html; charset=UTF-8');
+    expect(await res.text()).toMatch(/<a href='\/auth\/github'/);
     expect(res.status).toBe(200);
   });
 
-  test("ログイン時はユーザ名が表示される", async () => {
-    const app = require("./app");
-    const res = await app.request("/login");
+  test('ログイン時はユーザ名が表示される', async () => {
+    const app = require('./app');
+    const res = await app.request('/login');
     expect(await res.text()).toMatch(/testuser/);
     expect(res.status).toBe(200);
   });
 });
 
-describe("/logout", () => {
-  test("/ にリダイレクトされる", async () => {
-    const app = require("./app");
-    const res = await app.request("/logout");
-    expect(res.headers.get("Location")).toBe("/");
+describe('/logout', () => {
+  test('/ にリダイレクトされる', async () => {
+    const app = require('./app');
+    const res = await app.request('/logout');
+    expect(res.headers.get('Location')).toBe('/');
     expect(res.status).toBe(302);
   });
 });
 
-describe("/schedules", () => {
-  let scheduleId = "";
+describe('/schedules', () => {
+  let scheduleId = '';
   beforeAll(() => {
     mockIronSession();
   });
@@ -64,32 +64,32 @@ describe("/schedules", () => {
     await prisma.schedule.delete({ where: { scheduleId } });
   });
 
-  test("予定が作成でき、表示される", async () => {
+  test('予定が作成でき、表示される', async () => {
     await prisma.user.upsert({
       where: { userId: testUser.userId },
       create: testUser,
       update: testUser,
     });
 
-    const app = require("./app");
+    const app = require('./app');
 
-    const postRes = await app.request("/schedules", {
-      method: "POST",
+    const postRes = await app.request('/schedules', {
+      method: 'POST',
       body: new URLSearchParams({
-        scheduleName: "テスト予定1",
-        memo: "テストメモ1\r\nテストメモ2",
-        candidates: "テスト候補1\r\nテスト候補2\r\nテスト候補3",
+        scheduleName: 'テスト予定1',
+        memo: 'テストメモ1\r\nテストメモ2',
+        candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3',
       }),
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
     });
 
-    const createdSchedulePath = postRes.headers.get("Location");
+    const createdSchedulePath = postRes.headers.get('Location');
     expect(createdSchedulePath).toMatch(/schedules/);
     expect(postRes.status).toBe(302);
 
-    scheduleId = createdSchedulePath.split("/schedules/")[1];
+    scheduleId = createdSchedulePath.split('/schedules/')[1];
 
     const res = await app.request(createdSchedulePath);
     const body = await res.text();
